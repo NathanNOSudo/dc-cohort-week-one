2226bf29d9a34534ac7e23d8d26420112844c466
Nathan Orris <Nathan_NoSudo@codebloodeddev.com>
2020-03-31 13:49:02 -0500

    added python work from class

---
+++ dc-cohort-week-1/sample.py
@@ -0,0 +1,9 @@
+first_name = input('What is your first name?: ')
+last_name = input('What is your last name?: ')
+middle_name = 'Danger'
+
+print(first_name)
+print(middle_name)
+print(last_name)
+
+
