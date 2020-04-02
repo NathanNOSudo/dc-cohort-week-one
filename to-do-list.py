ac79c36b47d4b299ba7f7f49ac6c1d819f27b3b3
Nathan Orris <Nathan_NoSudo@codebloodeddev.com>
2020-04-02 18:38:57 -0500

    TODO list assignment 4

---
+++ to-do-list.py
@@ -0,0 +1,42 @@
+option = input("Press 1 to add tasks; Press 2 to delete tasks; Press 3 to view all tasks; Press q to quit: ")
+tasks = []
+
+while option != "q":
+  print("REMEMBER TO SANITIZE YOUR HANDS!")
+#User inputs 1 to add tasks
+  if option == "1":
+    title = input("Please input your task: ")
+    priority = input("Defcon3, Defcon2, or Defcon1 priority: ")
+
+    task = {
+      "title": title,
+      "priority": priority
+    }
+    # add dictionary to tasks[]
+    tasks.append(task)
+    print("DO NOT FORGET TO SANITIZE")    
+#User inputs 2 to delete tasks
+  elif option == "2":
+    for index in range(0, len(tasks)):
+      task_dict = tasks[index]
+      task_title = task_dict["title"]
+      task_priority = task_dict["priority"]
+      print(f"{index}  {task_title}  {task_priority}")
+      
+    task_to_delete = int(input("I hope its completed....no take backs! Input number corresponding to task to you wish to delete: "))
+
+    del tasks[task_to_delete]
+
+#User inputs 3 to view all tasks
+  elif option == "3":
+    for index in range(0, len(tasks)):
+      task_dict = tasks[index]
+      task_title = task_dict["title"]
+      task_priority = task_dict["priority"]
+      print(f"{index}  {task_title}  {task_priority}")
+      print("I HOPE YOU HAVE BEEN SANITIZING YOUR HANDS!")
+#if wrong button is pressed
+  #else:
+   # print("Please press 1, 2, 3, or q")
+
+  option = input("Press 1 to add tasks; Press 2 to delete tasks; Press 3 to view all tasks; Press q to quit: ")
