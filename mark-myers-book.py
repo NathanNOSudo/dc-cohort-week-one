00e34990a6b2bfb78ee99bcd03f21698ea5d686d
Nathan Orris <Nathan_NoSudo@codebloodeddev.com>
2020-04-03 14:44:40 -0500

    additonal for loops

--- mark-myers-book.py
+++ mark-myers-book.py
@@ -11,3 +11,20 @@ print(products)
 #loop through a list, displaying each element.
 for x in y:
   print(x)
+
+#loop with comparing if equal to a value
+for x in y:
+  if x == z:
+    print("ok")
+
+#for loop another example checking if an elements has the same valuew as variable my_street
+my_street = "Main"
+streets = ("Elm", "Walnut", "Center", "Main", "Front")
+for a_street in streets:
+  if a_street == my_street:
+    print("yes")
+
+#list with 3 elements. loop through and display each element.
+cars = ["Tesla", "Bolt", "Leaf"]
+for a_car in cars:
+  print(a_car)
