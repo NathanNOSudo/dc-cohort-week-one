7055e7aa2503c8be7ea7705424e602007da300f7
Nathan Orris <Nathan_NoSudo@codebloodeddev.com>
2020-04-03 14:17:17 -0500

    add deleting/removing by value example

--- mark-myers-book.py
+++ mark-myers-book.py
@@ -3,3 +3,7 @@ products = ["lamp", "table", "chair", "couch", "sink"]
 del products[4]
 print(products)
 
+#removes sink using its value. prints remaining items.
+products = ["lamp", "table", "chair", "couch", "sink"]
+products.remove("sink")
+print(products)
