f0a777c49f651800d52297f61d07b92cd83fd577
Nathan Orris <Nathan_NoSudo@codebloodeddev.com>
2020-04-05 01:26:59 -0500

    Draw a triangle and square

--- advanced projects/turttie.py
+++ advanced projects/turttie.py
@@ -1,14 +1,31 @@
 import turtle
+wn = turtle.Screen()             # Set up the window and its attributes
+wn.bgcolor("lightgreen")
 
-wn = turtle.Screen()
-wn.bgcolor("lightgreen")        # set the window background color
 
-tess = turtle.Turtle()
-tess.color("blue")              # make tess blue
-tess.pensize(3)                 # set the width of her pen
+tess = turtle.Turtle()           # create tess and set his pen width
+tess.pensize(5)
 
-tess.forward(50)
+alex = turtle.Turtle()           # create alex
+alex.color("hotpink")            # set his color
+
+tess.forward(80)                 # Let tess draw an equilateral triangle
 tess.left(120)
-tess.forward(50)
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)                   # complete the triangle
 
-wn.exitonclick()                # wait for a user click on the canvas
+tess.right(180)                  # turn tess around
+tess.forward(80)                 # move her away from the origin so we can see alex
+
+alex.forward(50)                 # make alex draw a square
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+
+wn.exitonclick()
